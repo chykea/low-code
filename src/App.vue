7025e9dd1f61c1375a3dd435099cc35b168ce037
chykea <9813494+chykea@user.noreply.gitee.com>
2022-07-30 20:34:45 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <h1>App组件---{{num}}</h1>
+  <button @click="add">点击+1</button>
 </template>
+<script setup>
+import {ref} from 'vue'
 
+let num = ref(0);
+function add(){
+  num.value+=1;
+}
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
