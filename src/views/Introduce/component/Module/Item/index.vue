9d95b9ed3f7d5ce854a6e02e9dc29cfc00a554b1
chykea <9813494+chykea@user.noreply.gitee.com>
2022-08-01 21:29:27 +0800

    列表(没有抠图)

--- src/views/Introduce/component/Module/Item/index.vue
+++ src/views/Introduce/component/Module/Item/index.vue
@@ -1,17 +1,40 @@
 <template>
     <div class="introduce-module-item">
         <div class="introduce-module-item-img">
-            <img src="" alt="">
+            <img :src="imgSrc" alt="">
         </div>
         <div class="introduce-module-item-title">
-            
+            <p>{{prop.title}}</p>
         </div>
         <div class="introduce-module-item-describe">
-
+            <p>{{prop.describe}}</p>
         </div>
     </div>
 </template>
 <script setup>
+import {defineProps} from 'vue'
+const prop = defineProps({
+    title:String,
+    describe:String,
+    imgSrc:String
+})
 </script>
 <style lang='scss' scoped>
+.introduce-module-item{
+    text-align: center;
+    width: 250px;
+    height: 230px;
+    .introduce-module-item-title{
+        &>p{
+            font-size: 18px;
+        }
+    }
+    .introduce-module-item-describe{
+        margin-top: 14px;
+        &>p{
+            font-size: 14px;
+            color: #7C7C7C;
+        }
+    }
+}
 </style>
