76ab4a43b70dc502a1d16b18aa83476779e1096a
zeng_jia_chun <2025904161@qq.com>
2022-08-14 11:31:48 +0800

    完善好的头部

--- src/views/PersonEdit/component/Header/index.vue
+++ src/views/PersonEdit/component/Header/index.vue
@@ -6,65 +6,97 @@
   </div>
   <div class="header-list">
     <ul>
-      <Item v-for="(item,index) in arr" :key="index" :title="item.title" :url="item.url"></Item>
+      <Item
+        v-for="(item, index) in arr"
+        :key="index"
+        :title="item.title"
+        :url="item.url"
+      ></Item>
     </ul>
-    <el-avatar :size="45" style="vertical-align: middle" />
+    <el-avatar :size="45" :src="imgavatar" class="user" />
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import Item from "./Item";
-const arr =ref([
-      {
-        url:'',
-        title:'新手教程'
-      },
-      {
-        url:'',
-        title:'项目管理'
-      },
-      {
-        url:'',
-        title:'素材库'
-      }
-    ]);
+import axios from "axios";
+import { getAdvator } from "@/utils/request.js";
+const arr = ref([
+  {
+    url: "/introduce",
+    title: "新手教程",
+  },
+  {
+    url: "/personedit",
+    title: "项目管理",
+  },
+  {
+    url: "",
+    title: "素材库",
+  },
+]);
+// const prop = defineProps({
+//   imgavatar: String,
+// });
+const imgavatar = ref("");  
+ axios({
+        method: "get",
+        url: "api/UserInfo/getAdvator",
+        headers: {
+            "Content-Type": "multipart/form-data",
+            token:
+                "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsaWQiOjIwMjIwODAzMSwiaXNzIjoiUml2ZW5ubiIsInVzZXJuYW1lIjoiUml2ZW42NiJ9.aadsQxU6mxk2gE3ivfDLW7EXuDJVODUgd0b-4gd3AMk",
+        },
+    }).then(
+        (res) => {
+            // console.log(res.data);
+            if (res.data.code == 200) {
+                imgavatar.value = "http://" + res.data.advator;
+            }
+        },
+        (err) => {
+            console.log(err.message);
+        }
+    );
 </script>
 
 <style scoped>
 img {
-    width: 22px;
-    height: 22px;
-  }
-  .LC-design {
-    display: inline-block;
-    width: 155px;
-    height: 28px;
-    margin-left: 21px;
-    font-size: 32px;
-    font-weight: bold;
-    line-height: 21px;
-    text-align: center;
-  }
-  .LC {
-    font-family: Berlin Sans FB Demi;
-    color: #17191a;
-  }
-  .design {
-    margin-left: 10px;
-    color: #aaa;
-  }
-  .header-list {
-    display: inline-block;
-    width: 540px;
-    height: inherit;
-    margin-left: 554px;
-    vertical-align: bottom;
-  }
-  .header-list ul {
-    /* padding: 0; */
-    list-style: none;
-    margin: 0;
-  }
- 
+  width: 22px;
+  height: 22px;
+}
+.LC-design {
+  display: inline-block;
+  width: 155px;
+  height: 28px;
+  margin-left: 21px;
+  font-size: 32px;
+  font-weight: bold;
+  line-height: 21px;
+  text-align: center;
+}
+.LC {
+  font-family: Berlin Sans FB Demi;
+  color: #17191a;
+}
+.design {
+  margin-left: 10px;
+  color: #aaa;
+}
+.header-list {
+  display: inline-block;
+  width: 500px;
+  height: inherit;
+  margin-left: 627px;
+  vertical-align: bottom;
+}
+.header-list ul {
+  /* padding: 0; */
+  list-style: none;
+  margin: 0;
+}
+.user {
+  vertical-align: middle;
+}
 </style>
