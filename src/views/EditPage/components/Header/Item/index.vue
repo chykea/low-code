f39d3c72941babf2fca9f821a6983b2c8b8cb0ec
ly14987 <1498742646@qq.com>
2022-08-22 02:27:32 +0800

    EditPage部分提交

---
+++ src/views/EditPage/components/Header/Item/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <!-- <a :href="url">{{prop.title}}</a> -->
+    <router-link :to="url" class="fonts">{{ prop.title }}</router-link>
+  </li>
+</template>
+
+<script setup>
+// -----
+import { defineProps } from "vue";
+const prop = defineProps({
+  title: String,
+  url: String,
+});
+</script>
+
+
+<style scoped>
+li {
+  float: left;
+  width: 72px;
+  /* height: 70x; */
+  margin-right: 78px;
+  /* line-height: 70px; */
+}
+.fonts {
+  font-size: 18px;
+  font-family: Microsoft YaHei;
+  font-weight: 400;
+  color: #17191a;
+  text-decoration: none;
+}
+</style>
---
+++ src/views/EditPage/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="PageOut">
+    <div class="head">
+      <el-header class="header">
+        <Header />
+      </el-header>
+    </div>
+    <div class="EditPage_selectArea">
+      <SelectArea></SelectArea>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// import { ref,reactive,toRaw, onMounted } from 'vue';
+
+import Header from "@/views/PersonEdit/component/Header/index.vue";
+import SelectArea from "./components/SelectArea/index.vue";
+// import {useStore} from 'vuex';
+</script>
+
+<style lang="scss">
+  .PageOut {
+    width: 1366px;
+    height: 100vh;
+    margin: 0 auto;
+  }
+  .head {
+    height: 73px;
+    padding-top: 21px;
+    background: #FFFFFF;
+    box-shadow: 0px 2px 21px 0px rgba(158,158,158,0.16);
+  }
+  .el-header {
+    --el-header-padding: 0 20px;
+    --el-header-height: 60px;
+    padding: var(--el-header-padding);
+    box-sizing: border-box;
+    flex-shrink: 0;
+    height: var(--el-header-height);
+  }
+  .header {
+    width: 1366px;
+    line-height: 60px;
+    margin: 0 auto;
+  }
+
+
+</style>
